dbdd10192d358782a67055b1ba2726e72fd98716
Dar Wright <elack33@gmail.com>
2015-08-17 20:45:23 -0700

    A bit more format change, moved title header down, got a round picture, melding with the video displays.

--- scripts/dw.js
+++ scripts/dw.js
@@ -5,26 +5,16 @@ $(document).ready(function() {
         //$('#vidHangman').removeClass('hidden');
         if ($('#vidHangman').hasClass( "hidden" ) === true && ($('#vidMovieSearch').hasClass('hidden') === false ||
             $('#vidTinyAdventureGame').hasClass('hidden') === false || $('#vidCarRemote').hasClass('hidden') === false)) {
-
             $('#vidHangman').removeClass('hidden');
             $('#vidMovieSearch').addClass('hidden');
             $('#vidTinyAdventureGame').addClass('hidden');
             $('#vidCarRemote').addClass('hidden');
-
             $('#space_Hangman').switchClass('spacer','selected');
             $('#space_MovieSearch').switchClass('selected', 'spacer');
             $('#space_TinyAdventureGame').switchClass('selected', 'spacer');
             $('#space_CarRemote').switchClass('selected', 'spacer');
-
         }
 
-        //else if ($('#vidHangman').hasClass( "hidden" ) === true){
-        //    console.log('truth!');
-        //    $('#vidHangman').removeClass('hidden');
-        //}
-        //else {
-        //    $('#vidHangman').addClass('hidden');
-        //}
     });
     $('#btnMovieSearch').click(function(event) {
         event.preventDefault();
@@ -35,19 +25,11 @@ $(document).ready(function() {
             $('#vidHangman').addClass('hidden');
             $('#vidTinyAdventureGame').addClass('hidden');
             $('#vidCarRemote').addClass('hidden');
-
             $('#space_MovieSearch').switchClass('spacer','selected');
             $('#space_Hangman').switchClass('selected', 'spacer');
             $('#space_TinyAdventureGame').switchClass('selected', 'spacer');
             $('#space_CarRemote').switchClass('selected', 'spacer');
         }
-
-        //else if ($('#vidMovieSearch').hasClass('hidden') === true) {
-        //    $('#vidMovieSearch').removeClass('hidden');
-        //}
-        //else {
-        //    $('#vidMovieSearch').addClass('hidden');
-        //}
     });
     $('#btnTinyAdventureGame').click(function(event) {
         event.preventDefault();
@@ -58,19 +40,11 @@ $(document).ready(function() {
             $('#vidHangman').addClass('hidden');
             $('#vidMovieSearch').addClass('hidden');
             $('#vidCarRemote').addClass('hidden');
-
             $('#space_TinyAdventureGame').switchClass('spacer','selected');
             $('#space_Hangman').switchClass('selected', 'spacer');
             $('#space_MovieSearch').switchClass('selected', 'spacer');
             $('#space_CarRemote').switchClass('selected', 'spacer');
         }
-
-        //else if ($('#vidTinyAdventureGame').hasClass('hidden') === true) {
-        //    $('#vidTinyAdventureGame').removeClass('hidden');
-        //}
-        //else {
-        //    $('#vidTinyAdventureGame').addClass('hidden');
-        //}
     });
     $('#btnCarRemote').click(function(event) {
         event.preventDefault();
@@ -81,19 +55,11 @@ $(document).ready(function() {
             $('#vidHangman').addClass('hidden');
             $('#vidMovieSearch').addClass('hidden');
             $('#vidTinyAdventureGame').addClass('hidden');
-
             $('#space_CarRemote').switchClass('spacer','selected');
             $('#space_Hangman').switchClass('selected', 'spacer');
             $('#space_MovieSearch').switchClass('selected', 'spacer');
             $('#space_TinyAdventureGame').switchClass('selected', 'spacer');
         }
-
-        //else if ($('#vidCarRemote').hasClass('hidden') === true) {
-        //    $('#vidCarRemote').removeClass('hidden');
-        //}
-        //else {
-        //    $('#vidCarRemote').addClass('hidden');
-        //}
     });
 });
 
